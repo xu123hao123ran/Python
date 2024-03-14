34f8370c7fa0b121d576de307f9a72e8214fee35
xu123hao123ran <2380414408@qq.com>
2024-03-14 20:10:18 +0800

    修改文件shell_sort.py

--- 排序/shell_sort.py
+++ 排序/shell_sort.py
@@ -1,8 +1,13 @@
 def shell_sort(alist):
     n = len(alist)
-    gap = n//2
-    i = 1
-    if alist[i] < alist[i-gap]:
-        alist[i], alist[i-1] = alist[i-1], alist[i]
-    else:
-        print('1')
+    gap = n // 2
+    while gap >= 1:
+        for j in range(gap, n):
+            i = j
+            while i > 0:
+                if alist[i] < alist[i - gap]:
+                    alist[i], alist[i - gap] = alist[i - gap], alist[i]
+                    i -= gap
+                else:
+                    break
+        gap = gap // 2
